d778afcf5e4524a731dfabd08f5143c86bf6d1ff
thorsten <tk@system42.io>
2023-03-06 15:23:36 +0100

    Fix

--- .github/scripts/makemsi.ps1
+++ .github/scripts/makemsi.ps1
@@ -3,35 +3,35 @@ Write-Output "-----------------"
 $ErrorActionPreference = 'Stop'
 Get-ChildItem env:
 
-Write-Output "Install goversioninfo"
-#go install github.com/josephspurrier/goversioninfo/cmd/goversioninfo
+Write-Output "[*] Install goversioninfo"
+go install github.com/josephspurrier/goversioninfo/cmd/goversioninfo
 
-Write-Output "Install WIX"
+Write-Output "[*] Install WIX"
 choco install wixtoolset
 
-Write-Output "Building MSI"
-#Get-Content new.json > cmd\rport\versioninfo.json
-#Write-Output "Version the client with whatever is in versioninfo.json"
-#go generate cmd/rport/main.go
-Write-Output "Build rport client for windows"
-go build -ldflags "-s -w -X github.com/cloudradar-monitoring/rport/share.BuildVersion=$($env:GITHUB_REF_NAME)" -o rport.exe ./cmd/rport/...
-Get-ChildItem
+Write-Output "[*] Building MSI"
+Get-Content new.json > cmd\rport\versioninfo.json
+Write-Output "Version the client with whatever is in versioninfo.json"
+go generate cmd/rport/main.go
+Write-Output "[*] Build rport client for windows"
+go build -ldflags "-s -w -X {{.Env.PROJECT}}/share.BuildVersion={{.Version}}" -o rport.exe ./cmd/rport/...
+Get-ChildItem -File *.exe
 .\rport.exe --version
 
-Write-Output "Creates wixobj's"
+Write-Output "[*] Creating wixobj's"
 & 'C:\Program Files (x86)\WiX Toolset v3.11\bin\candle.exe' -dPlatform=x64 -ext WixUtilExtension opt/resource/*.wxs
-Write-Output "creates MSI"
+Write-Output "[*] Creating MSI"
 & 'C:\Program Files (x86)\WiX Toolset v3.11\bin\light.exe' -loc opt/resource/Product_en-us.wxl -ext WixUtilExtension -ext WixUIExtension -sval -out rport-client.msi LicenseAgreementDlg_HK.wixobj WixUI_HK.wixobj Product.wixobj
-Get-ChildItem
+Get-ChildItem -File *.msi
 
-Write-Output "Creating a self signed certificate"
+Write-Output "[*] Creating a self signed certificate"
 $cert = New-SelfSignedCertificate -DnsName rport.io -CertStoreLocation cert:\LocalMachine\My -type CodeSigning
 $MyPassword = ConvertTo-SecureString -String "MyPassword" -Force -AsPlainText
 Export-PfxCertificate -cert $cert -FilePath mycert.pfx -Password $MyPassword
 
-Write-Output "Signing the generated MSI"
+Write-Output "[*] Signing the generated MSI"
 & 'C:\Program Files (x86)\Windows Kits\10\bin\10.0.22621.0\x86\signtool.exe' sign /fd SHA256 /f mycert.pfx /p MyPassword rport-client.msi
 
-Write-Output "Uploading MSI to download server"
+Write-Output "[*] Uploading MSI to download server"
 $file = "rport-client.msi"
 & curl.exe -fs https://$($env:DOWNLOAD_SERVER)/exec/upload.php -H "Authentication: $($env:MSI_UPLOAD_TOKEN)" -F file=@$($file) -F dest_dir="rport/unstable/msi"
