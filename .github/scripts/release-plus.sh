270bf0cc65fe1e512daa9163d16c661b2fb5f84d
thorsten <tk@system42.io>
2023-03-07 16:40:42 +0100

    fix

--- .github/scripts/release-plus.sh
+++ .github/scripts/release-plus.sh
@@ -10,10 +10,10 @@ else
   echo "⛔ ~/.plus.env not found. Exit"
   false
 fi
-ds rport-plus
+cd rport-plus
 export PLUS_ARTIFACT=rport-plus_${PLUS_LATEST}@${GITHUB_REF_NAME}_Linux_$(uname -m).tar.gz
+echo "🚚Will create ${PLUS_ARTIFACT} release file"
 echo "rport-plus v${PLUS_LATEST}; compiled for rportd ${GITHUB_REF_NAME}; built on $(date)" > version.txt
-echo "Will create ${PLUS_ARTIFACT} now"
 tar czf ${PLUS_ARTIFACT} README.md license.txt version.txt rport-plus.so
 tar tzf ${PLUS_ARTIFACT}
 ls -la ${PLUS_ARTIFACT}
